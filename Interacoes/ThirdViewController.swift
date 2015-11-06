a298ba8b9d12676273afa891284967a29b74c734
Nylon <diego.gomes87@gmail.com>
2015-11-06 16:51:25 -0200

    Simples uso de  tabbed application

--- Interacoes/ThirdViewController.swift
+++ Interacoes/ThirdViewController.swift
@@ -1,26 +1,27 @@
 //
-//  FirstViewController.swift
+//  ThirdViewController.swift
 //  Interacoes
 //
 //  Created by Diego Gomes on 06/11/2015.
 //  Copyright (c) 2015 Nylon. All rights reserved.
 //
 
+
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class ThirdViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("i have scare")
+        println("here")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
